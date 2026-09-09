b8d16dcb1f971a7f5c12f270fd6fc203a3857d2b
kartikmehta-cft <kartikmehtamongooseintern@gmail.com>
2026-09-09 13:11:48 +0530

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -26,22 +26,50 @@ def call(Map config = [:]) {
     echo "Credential: ${sshCredId}"
 
     stage('SSH Test') {
-        sshagent([sshCredId]) {
-            sh """
+
+        withCredentials([
+            sshUserPrivateKey(
+                credentialsId: sshCredId,
+                keyFileVariable: 'SSH_KEY',
+                usernameVariable: 'SSH_USER'
+            )
+        ]) {
+
+            sh '''
+                chmod 600 "$SSH_KEY"
+
                 ssh -o StrictHostKeyChecking=no \
-                deployer@${host} \
-                'whoami && hostname && docker ps'
-            """
+                    -i "$SSH_KEY" \
+                    "$SSH_USER@$TARGET_HOST" \
+                    "whoami && hostname && docker ps"
+            '''
         }
     }
 
     stage('Docker Compose Deploy') {
-        sshagent([sshCredId]) {
-            sh """
-                ssh -o StrictHostKeyChecking=no \
-                deployer@${host} \
-                'cd ${deployPath} && docker compose up -d --build'
-            """
+
+        withCredentials([
+            sshUserPrivateKey(
+                credentialsId: sshCredId,
+                keyFileVariable: 'SSH_KEY',
+                usernameVariable: 'SSH_USER'
+            )
+        ]) {
+
+            withEnv([
+                "TARGET_HOST=${host}",
+                "DEPLOY_PATH=${deployPath}"
+            ]) {
+
+                sh '''
+                    chmod 600 "$SSH_KEY"
+
+                    ssh -o StrictHostKeyChecking=no \
+                        -i "$SSH_KEY" \
+                        "$SSH_USER@$TARGET_HOST" \
+                        "cd '$DEPLOY_PATH' && docker compose up -d --build"
+                '''
+            }
         }
     }
 }
